8f58fef1ec435c4cc64d0d31e46b61cb67b2e1fc
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-04-06 01:55:42 +0530

    create ci.yml

--- src/main/java/com/alextechsolutions/serviceaheadversion5_0/Model/Users.java
+++ src/main/java/com/alextechsolutions/serviceaheadversion5_0/Model/Users.java
@@ -7,12 +7,10 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.stereotype.Component;
 
-@Component
+
+
 @Entity
-@Getter
-@Setter
 @ToString
 public class Users {
 
@@ -21,4 +19,26 @@ public class Users {
     private int id;
     private String username;
     private String password;
+
+        public String getPassword() {
+            return password;
+        }
+
+        // Setter for password
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        // Getter for username
+        public String getUsername() {
+            return username;
+        }
+
+        // Setter for username
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        // Other fields and methods...
+
 }
